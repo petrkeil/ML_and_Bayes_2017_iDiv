e23a882d17b4a4d60459c9b274b9e1518115a981
petrkeil <pkeil@seznam.cz>
2017-02-24 15:29:22 +0100

    regular commit

---
+++ Univariate Poisson Model/plotting_poisson.r
@@ -0,0 +1,17 @@
+
+move.poisson <- function(lambda)
+{
+  p.mass <- dpois(0:100, lambda=lambda)
+  plot(0:100, p.mass, ylim=c(0,0.2),
+       col="red", pch=19, 
+       ylab="Probability mass",
+       xlab="x")
+}
+
+
+plot.poisson.deviance <- function(x, lambda)
+{
+  deviance <- round(deviance.function(x, lambda),2)
+  hist(x, main=paste("Deviance =", deviance), freq = FALSE, ylim=c(0,0.3), col="grey")
+  points(0:20, dpois(0:20, lambda), pch=19, col="red")
+}
